5087297fd911d4484cdbe97a6ef724da2f2bf500
rubenesque <a.ruben00001@gmail.com>
2025-07-15 15:10:01 +0100

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts" context="module">
-	import { Basket, Footprints, Plant, Quotes, Tree, UsersThree, Wind } from 'phosphor-svelte';
+	import {
+		ArrowRight,
+		Basket,
+		Footprints,
+		Plant,
+		Quotes,
+		Tree,
+		UsersThree,
+		Wind
+	} from 'phosphor-svelte';
 
 	import image from '^assets/image';
 
@@ -26,7 +35,7 @@
 	</h3> -->
 </div>
 
-<div class="mt-16 grid grid-cols-3 gap-10 px-52">
+<div class="mt-32 grid grid-cols-3 gap-10 px-52">
 	<div class="relative">
 		<enhanced:img
 			class="aspect-[9/10] rounded-md object-cover object-[30%_50%]"
@@ -35,15 +44,20 @@
 		/>
 
 		<div
-			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col justify-end rounded-b-md bg-gradient-to-t to-transparent p-2"
+			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col items-center justify-end overflow-hidden rounded-b-md bg-gradient-to-t to-transparent p-2"
 		>
-			<p class="flex flex-col text-center text-[22px]">
-				<span class="text-bc-amber scale-y-[-1] rotate-180 self-start"
-					><Quotes weight="fill" /></span
-				>
-				<span class="text-white">I feel I've left feeling more accepting of myself.</span>
-				<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
-			</p>
+			<div class="flex h-[150px] flex-col justify-end overflow-hidden">
+				<p class="flex flex-col text-center text-[22px]">
+					<span class="text-bc-amber scale-y-[-1] rotate-180 self-start text-[22px]"
+						><Quotes weight="fill" /></span
+					>
+					<span class="line-clamp-2 text-white"
+						>I feel I've left feeling more accepting of myself.
+					</span>
+					<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
+				</p>
+				<p class="shrink-0 pb-2 text-center text-lg text-white">Alissa - Participant</p>
+			</div>
 		</div>
 	</div>
 
@@ -55,18 +69,24 @@
 		/>
 
 		<div
-			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col justify-end rounded-b-md bg-gradient-to-t to-transparent p-2"
+			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col items-center justify-end overflow-hidden rounded-b-md bg-gradient-to-t to-transparent p-2"
 		>
-			<p class="flex flex-col text-center text-[22px]">
-				<span class="text-bc-amber scale-y-[-1] rotate-180 self-start"
-					><Quotes weight="fill" /></span
-				>
-				<span class="text-white"
-					>The Birch Collective has been a transformative experience, both for the individuals I
-					care for and for myself as a caregiver.</span
-				>
-				<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
-			</p>
+			<div class="flex h-[150px] flex-col justify-end overflow-hidden">
+				<p class="flex flex-col text-center text-[22px]">
+					<span class="text-bc-amber scale-y-[-1] rotate-180 self-start text-[22px]"
+						><Quotes weight="fill" /></span
+					>
+					<!-- <span class="text-white"
+						>The Birch Collective has been a transformative experience, both for the individuals I
+						care for and for myself as a caregiver.</span
+					> -->
+					<span class="line-clamp-2 text-white"
+						>The Birch Collective has been a transformative experience, both for the individuals I
+					</span>
+					<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
+				</p>
+				<p class="shrink-0 pb-2 text-center text-lg text-white">Pauline - Support Worker</p>
+			</div>
 		</div>
 	</div>
 
@@ -78,56 +98,66 @@
 		/>
 
 		<div
-			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col justify-end rounded-b-md bg-gradient-to-t to-transparent p-2"
+			class="from-bc-slate-pine absolute bottom-0 left-0 z-10 flex h-1/2 w-full flex-col items-center justify-end overflow-hidden rounded-b-md bg-gradient-to-t to-transparent p-2"
 		>
-			<p class="flex flex-col text-center text-[22px]">
-				<span class="text-bc-amber scale-y-[-1] rotate-180 self-start"
-					><Quotes weight="fill" /></span
-				>
-				<span class="text-white"
-					>The time I got to spend with others on the camp was just something so magical and
-					wonderful and beautiful.</span
-				>
-				<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
-			</p>
+			<div class="flex h-[150px] flex-col justify-end overflow-hidden">
+				<p class="flex flex-col text-center text-[22px]">
+					<span class="text-bc-amber scale-y-[-1] rotate-180 self-start text-[22px]"
+						><Quotes weight="fill" /></span
+					>
+					<span class="line-clamp-2 text-white">
+						The time I got to spend with others on the camp was just something so magical and
+						wonderful and beautiful.
+					</span>
+					<span class="text-bc-amber self-end"><Quotes weight="fill" /></span>
+				</p>
+				<p class="shrink-0 pb-2 text-center text-lg text-white">Jo - Participant</p>
+			</div>
 		</div>
 	</div>
 </div>
 
 <section class="mt-32">
-	<h2 class="font-display text-bc-slate-pine text-center text-6xl font-bold">What We Do</h2>
+	<h2 class="font-display text-bc-slate-pine text-center text-7xl font-bold">
+		Cultivating Purpose, Belonging and Resilience
+	</h2>
 
-	<div class="mt-8 flex justify-center">
-		<div class="flex max-w-[700px] flex-col gap-4">
-			<div class="flex items-center gap-4 text-xl">
-				<div class="text-bc-amber text-4xl">
+	<div class="mt-12 flex justify-center">
+		<div class="flex max-w-[700px] flex-col gap-6 leading-relaxed">
+			<div class="flex flex-col items-center gap-2 text-2xl">
+				<div class="text-bc-amber rounded-md p-1 text-4xl">
 					<Footprints />
 				</div>
-				<p>We work alongside you to empower and support your transition into adulthood.</p>
+				<p class="text-center font-light">
+					We walk alongside you to empower and support your transition into adulthood.
+				</p>
 			</div>
-			<div class="flex items-center gap-4 text-xl">
+
+			<div class="flex flex-col items-center gap-2 text-2xl">
 				<div class="text-bc-amber text-4xl">
 					<Wind />
 				</div>
-				<p>
+				<p class="text-center font-light">
 					We want you to unlock your potential to engage fully with life, and to respond resiliently
 					to life's challenges.
 				</p>
 			</div>
-			<div class="flex items-center gap-4 text-xl">
+
+			<div class="flex flex-col items-center gap-4 text-2xl">
 				<div class="text-bc-amber text-4xl">
 					<Tree />
 				</div>
-				<p>
+				<p class="text-center font-light">
 					We believe that developing connections with the natural world gives you a sense of
 					belonging, and helps you find purpose in life.
 				</p>
 			</div>
-			<div class="flex items-center gap-4 text-xl">
+
+			<div class="flex flex-col items-center gap-4 text-2xl">
 				<div class="text-bc-amber text-4xl">
 					<UsersThree />
 				</div>
-				<p>
+				<p class="text-center font-light">
 					We offer you the chance to be part of an inclusive community where you can explore new
 					found skills and interests.
 				</p>
@@ -136,29 +166,33 @@
 	</div>
 </section>
 
-<section class="mt-20 grid px-52">
-	<h2 class="font-display text-bc-slate-pine text-center text-6xl font-bold">Programmes</h2>
+<section class="mt-24 grid px-52">
+	<h2 class="font-display text-bc-slate-pine text-center text-7xl font-bold">Programmes</h2>
 
-	<div class="flex justify-center">
-		<p class="mt-4 max-w-[700px] text-center text-xl">
+	<!-- <div class="flex justify-center">
+		<p class="mt-6 max-w-[700px] text-center text-2xl font-light">
 			We have a range of offers to suit your needs, from online courses to weekly hang outs and
 			residential programmes.
 		</p>
-	</div>
+	</div> -->
 
-	<div class="mt-8 grid grid-cols-3 gap-10">
+	<div class="mt-12 grid grid-cols-3 gap-10">
 		<div>
 			<div>
-				<enhanced:img class="aspect-video object-cover" src={image.placeholder.chillies} alt="" />
+				<enhanced:img
+					class="aspect-video rounded-md object-cover"
+					src={image.placeholder.chillies}
+					alt=""
+				/>
 			</div>
-			<h4 class="font-display text-bc-amber mt-4 text-4xl font-bold tracking-wide">
+			<h4 class="font-display text-bc-amber mt-4 text-5xl font-bold tracking-wide">
 				Fresh Air Thursday
 			</h4>
-			<p class="mt-4 text-lg">
+			<p class="mt-4 text-xl font-light">
 				A free weekly group for 16-25 year olds, Just a 5-minute walk form St George's Park!
 			</p>
 
-			<div class="mt-4 text-lg">
+			<div class="mt-4 text-xl font-light">
 				<p>Includes:</p>
 				<div class="mt-2 flex items-center gap-2">
 					<span class="text-bc-slate-pine">
@@ -173,26 +207,33 @@
 					<p>Nature based craft</p>
 				</div>
 			</div>
+
+			<div
+				class="bg-bc-slate-pine mt-6 inline-flex items-center gap-2 self-start rounded-md px-2 py-1 font-medium text-white uppercase"
+			>
+				<span>Find out more</span>
+				<span><ArrowRight /></span>
+			</div>
 		</div>
 
 		<div>
 			<div>
 				<enhanced:img
-					class="aspect-video object-cover"
+					class="aspect-video rounded-md object-cover"
 					src={image.placeholder.seeding_change}
 					alt=""
 				/>
 			</div>
-			<h4 class="font-display text-bc-amber mt-4 text-4xl font-bold tracking-wide">
+			<h4 class="font-display text-bc-amber mt-4 text-5xl font-bold tracking-wide">
 				Seeding Change
 			</h4>
-			<p class="mt-4 text-lg">
+			<p class="mt-4 line-clamp-2 text-xl font-light">
 				A FREE camp for 18–25-year-olds who are feeling low, unmotivated, or ready for a fresh start
 				— connect with nature, yourself, and others, just 35 minutes from Bristol.
 			</p>
 
-			<div class="mt-4 text-lg">
-				<p>This programme involves:</p>
+			<div class="mt-4 text-xl font-light">
+				<p>Includes:</p>
 				<div class="mt-2 flex items-center gap-2">
 					<span class="text-bc-slate-pine">
 						<Plant />
@@ -205,26 +246,33 @@
 					</span>
 					<p>Nature based craft</p>
 				</div>
+			</div>
+
+			<div
+				class="bg-bc-slate-pine mt-6 inline-flex items-center gap-2 self-start rounded-md px-2 py-1 font-medium text-white uppercase"
+			>
+				<span>Find out more</span>
+				<span><ArrowRight /></span>
 			</div>
 		</div>
 
 		<div>
 			<div>
 				<enhanced:img
-					class="aspect-video object-cover"
+					class="aspect-video rounded-md object-cover"
 					src={image.placeholder.therapeutic_forest_school}
 					alt=""
 				/>
 			</div>
-			<h4 class="font-display text-bc-amber mt-4 text-4xl font-bold tracking-wide">
-				Therapeutic Forest School
+			<h4 class="font-display text-bc-amber mt-4 text-5xl font-bold tracking-wide">
+				Seeding Change
 			</h4>
-			<p class="mt-4 text-lg">
+			<p class="mt-4 line-clamp-2 text-xl font-light">
 				Tailored programs for specific groups, co-designed with partner organisations.
 			</p>
 
-			<div class="mt-4 text-lg">
-				<p>This programme involves:</p>
+			<div class="mt-4 text-xl font-light">
+				<p>Includes:</p>
 				<div class="mt-2 flex items-center gap-2">
 					<span class="text-bc-slate-pine">
 						<Plant />
@@ -237,13 +285,20 @@
 					</span>
 					<p>Nature based craft</p>
 				</div>
+			</div>
+
+			<div
+				class="bg-bc-slate-pine mt-6 inline-flex items-center gap-2 self-start rounded-md px-2 py-1 font-medium text-white uppercase"
+			>
+				<span>Find out more</span>
+				<span><ArrowRight /></span>
 			</div>
 		</div>
 	</div>
 </section>
 
-<section class="mt-20 grid px-52">
-	<h2 class="font-display text-bc-slate-pine text-center text-6xl font-bold">Support Us</h2>
+<section class="mt-24 grid px-52">
+	<h2 class="font-display text-bc-slate-pine text-center text-7xl font-bold">Support Us</h2>
 
 	<div class="flex justify-center">
 		<p class="mt-4 max-w-[700px] text-center text-xl">
