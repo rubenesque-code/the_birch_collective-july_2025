3458fbd5905f154d92c3d7165544f636223b04fc
rubenesque <a.ruben00001@gmail.com>
2025-08-13 16:53:16 +0100

    video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,6 @@
 	import { Header, OurMissionModal } from '^components/~sections';
 	import { updateSiteState } from '^lib/state';
 
-	// need distinction between paid and free.
 	// programmes (for individuals to sign up to); 1:1 mentoring; tailored programs for groups; youth advisory board
 	// for young people: group based nature programmes; 1:1 mentoring; youth advisory board; non-official volunteering (as mentor or something like that)?
 	// for institution: tailored programs.
