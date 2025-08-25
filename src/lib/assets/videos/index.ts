c2f4393863858f5690a613fd866bd6fcbe0ac153
rubenesque <a.ruben00001@gmail.com>
2025-08-25 13:33:43 +0100

    using lfs

--- src/lib/assets/videos/index.ts
+++ src/lib/assets/videos/index.ts
@@ -1,5 +1,4 @@
 import bannerVideoMp4 from "../_files/videos/banner.mp4";
-import bannerVideoWebm from "../_files/videos/banner.mp4";
 import mission from "../_files/videos/mission.mp4";
 
-export { bannerVideoMp4, bannerVideoWebm, mission }
+export { bannerVideoMp4, mission }
