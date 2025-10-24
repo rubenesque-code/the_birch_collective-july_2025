779c51f72b823fa43bdd7702a546b8ada503e9a1
rubenesque <a.ruben00001@gmail.com>
2025-10-24 17:41:56 +0100

    free programme css and component refactor

--- src/lib/components/~sections/image-gallery-dialog/+component.svelte
+++ src/lib/components/~sections/image-gallery-dialog/+component.svelte
@@ -18,15 +18,17 @@
 	}}
 >
 	<Dialog.Content
-		class="flex h-[1600px] max-h-[calc(100vh-24px)] !w-[1600px] !max-w-[calc(100vw-24px)] flex-col !p-4 sm:max-h-[calc(100vh-40px)] sm:!max-w-[calc(100vw-40px)] sm:!p-6 md:!p-12"
+		class="flex h-[1600px] max-h-[calc(100vh-24px)] !w-[1600px] !max-w-[calc(100vw-24px)] flex-col !p-4  md:!pt-12 lg:max-h-[calc(100vh-40px)] lg:!max-w-[calc(100vw-40px)] lg:!p-12"
 	>
-		<Dialog.Header class="flex shrink-0 flex-col items-center pb-1 text-center">
+		<Dialog.Header
+			class="absolute top-4 left-4 flex shrink-0 flex-col items-center pb-1 text-center"
+		>
 			<Dialog.Title class="!text-base md:text-lg"
 				>Images from <span class="text-bc-amber font-medium italic">Fresh</span></Dialog.Title
 			>
 		</Dialog.Header>
 
-		<Carousel.Root opts={{ loop: true, align: 'center' }} class="flex h-full w-full grow flex-col">
+		<Carousel.Root opts={{ loop: true, align: 'center' }} class="flex h-full w-full grow flex-col ">
 			<Carousel.Content class="ml-0 !h-full w-full grow !pl-0" hiddenParentClass="h-full p-0 !pl-0">
 				{#each images as item}
 					<Carousel.Item class="grabbable h-full w-full cursor-grab  !pl-0 active:cursor-grabbing">
@@ -35,7 +37,7 @@
 				{/each}
 			</Carousel.Content>
 
-			<div class="mt-3 flex shrink-0 items-center justify-between sm:py-4 md:py-6">
+			<div class="mt-3 flex shrink-0 items-center justify-between xl:py-6">
 				<Carousel.CarouselPrevious class="static translate-y-0 cursor-pointer" />
 
 				<Carousel.CarouselNext class="static translate-y-0 cursor-pointer" />
